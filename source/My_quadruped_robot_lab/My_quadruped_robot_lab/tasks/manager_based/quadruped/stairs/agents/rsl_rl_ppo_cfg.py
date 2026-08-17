7f82f0b0153091b97e891153bbf878cb41ee5828
liming <778003552@qq.com>
2026-08-17 16:56:38 +0800

    0817_3

--- source/My_quadruped_robot_lab/My_quadruped_robot_lab/tasks/manager_based/quadruped/stairs/agents/rsl_rl_ppo_cfg.py
+++ source/My_quadruped_robot_lab/My_quadruped_robot_lab/tasks/manager_based/quadruped/stairs/agents/rsl_rl_ppo_cfg.py
@@ -28,7 +28,7 @@ class OpenRobotStairsPPORunnerCfg(RslRlOnPolicyRunnerCfg):
         num_learning_epochs=5,
         num_mini_batches=4,
         learning_rate=1.0e-5,
-        schedule="adaptive",
+        schedule="fixed",
         gamma=0.99,
         lam=0.95,
         desired_kl=0.01,
